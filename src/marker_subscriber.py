59e3325902f811743d0941193e66e8011ef6b199
Sam Lensgraf <sam.lensgraf@gmail.com>
2020-02-06 19:16:51 +0000

    add output files for two images on raspberry pi 4b

--- src/marker_subscriber.py
+++ src/marker_subscriber.py
@@ -8,12 +8,20 @@ CsvRow = collections.namedtuple("CsvRow", ["frame_number", "received_time", "sen
 
 collected_data = []
 output_file = ""
+number_frames = 0
+
+number_frames_collected = 0
 
 def marker_callback(marker_message):
-    global collected_data
+    global collected_data, number_frames_collected, number_frames
+
+    if number_frames_collected > number_frames:
+        print "Got all frames. Exiting."
+        rospy.signal_shutdown(5)
+
     received_time = rospy.Time.now()
     delay = (received_time - marker_message.header.stamp).to_sec()
-    print "Frame {marker_seq} delay {delay}".format(marker_seq=marker_message.header.seq, delay=delay)
+    print "Frame {marker_seq} at {received_time} delay: {delay}".format(marker_seq=marker_message.header.seq, received_time=received_time, delay=delay)
 
     data_row = CsvRow(
         frame_number=marker_message.header.seq,
@@ -23,6 +31,8 @@ def marker_callback(marker_message):
         number_markers=len(marker_message.markers)
     )
     collected_data.append(data_row)
+
+    number_frames_collected += 1
 
 
 def shutdown_handler():
@@ -34,7 +44,7 @@ def shutdown_handler():
 
 
 def main():
-    global output_file, collected_data
+    global output_file, collected_data, number_frames
 
     rospy.init_node("publisher_node")
     rospy.loginfo("Starting data logger.")
@@ -43,6 +53,8 @@ def main():
     output_file = rospy.get_param("~output_file")
 
     marker_topic = rospy.get_param("~ar_tag_topic")
+
+    number_frames = rospy.get_param("~number_frames")
     rospy.Subscriber(marker_topic, ar_track_alvar_msgs.msg.AlvarMarkers, marker_callback)
 
     rospy.spin()
